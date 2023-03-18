5dd57945b322ff3522459af75a64f06f7eaae418
lacnoskillz <kaikane3457@hotmail.com>
2023-03-18 03:10:13 -0500

    imported new ADD_USER query instead of old createUser function using useMutation which was also imported

--- Develop/client/src/components/LoginForm.js
+++ Develop/client/src/components/LoginForm.js
@@ -4,7 +4,9 @@ import { Form, Button, Alert } from 'react-bootstrap';
 
 import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
-
+//import LOGIN_USER Query and UseMutation from apollo
+import { LOGIN_USER } from '../utils/mutations';
+import { useMutation } from '@apollo/client';
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
@@ -26,7 +28,8 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      //changed old function for new querys
+      const response = await useMutation(LOGIN_USER);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
--- Develop/client/src/components/SignupForm.js
+++ Develop/client/src/components/SignupForm.js
@@ -1,8 +1,12 @@
+
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
 import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
+//import ADD_USER query and useMutation 
+import { ADD_USER } from '../utils/mutations';
+import { useMutation } from '@apollo/client';
 
 const SignupForm = () => {
   // set initial form state
@@ -28,7 +32,7 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const response = await useMutation(ADD_USER);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
