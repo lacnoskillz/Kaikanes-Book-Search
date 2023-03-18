461e667f183d1152bf523a3815746c923fea30f0
lacnoskillz <kaikane3457@hotmail.com>
2023-03-18 01:40:09 -0500

    added query to get books and find single User. made mutations to create new user. Login. saveBook and removeBook

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -6,13 +6,54 @@ const resolvers = {
       Book: async () => {
         return Book.find({});
       },
-      Users: async (parent, { _id }) => {
+      User: async (parent, { _id }) => {
         const params = _id ? { _id } : {};
-        return User.find(params);
+        return User.findOne(params);
       },
     },
     Mutation: {
-   
+      createUser: async (parent, { username, email, password }) => {
+        const user = await User.create({ username, email, password });
+        const token = signToken(User);
+  
+        return { token, user };
+      },
+      login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+  
+        if (!user) {
+          throw new AuthenticationError('No profile with this email found!');
+        }
+  
+        const correctPw = await user.isCorrectPassword(password);
+  
+        if (!correctPw) {
+          throw new AuthenticationError('Incorrect password!');
+        }
+  
+        const token = signToken(user);
+        return { token, user };
+      },
+  
+      saveBook: async (parent, { User_Id, book }) => {
+        return User.findOneAndUpdate(
+          { _id: User_Id },
+          {
+            $addToSet: { savedBook: book },
+          },
+          {
+            new: true,
+            runValidators: true,
+          }
+        );
+      },
+      removeBook: async (parent, { User_Id, book }) => {
+        return User.findOneAndUpdate(
+          { _id: User_Id },
+          { $pull: { savedBook: book } },
+          { new: true }
+        );
+      },
     },
   };
   
