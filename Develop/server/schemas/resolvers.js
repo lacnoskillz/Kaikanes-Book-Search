e9f4474bd7db1af64ca1664f4679634997a0b5fc
lacnoskillz <kaikane3457@hotmail.com>
2023-03-21 18:53:13 -0500

    fixed typo in resolvers.js line 14 signToken(User) to (user)

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -11,7 +11,7 @@ const resolvers = {
     Mutation: {
       addUser: async (parent, { username, email, password }) => {
         const user = await User.create({ username, email, password });
-        const token = signToken(User);
+        const token = signToken(user);
   
         return { token, user };
       },
