072d51be27f04c3b6802e8b42bb9e44a9ce52236
lacnoskillz <kaikane3457@hotmail.com>
2023-03-21 18:41:41 -0500

    added appollo dependecnys in app.js fixed typos

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
+const { ApolloServer, AuthenticationError } = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
 const { authMiddleware } = require('./utils/auth');
